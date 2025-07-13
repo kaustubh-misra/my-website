49e11a3cdffbad971841d0aa867d23bdb992c0f6
kaustubh-misra <kaustubh@samagata.org>
2025-07-13 16:15:05 +0530

    photo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/kaustubh-misra',
+  assetPrefix: '/kaustubh-misra/',
 };
 
 module.exports = nextConfig; 
